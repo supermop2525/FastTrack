f529cdba78a2ccec0bfd5d50079bf6c053fb79ba
supermop2525 <omair.sandhu@ryerson.ca>
2018-11-10 22:23:50 -0500

    new layout

--- app/src/main/java/com/example/omair/hcapp/MainActivity.java
+++ app/src/main/java/com/example/omair/hcapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.*;
 import android.content.*;
 import android.net.*;
 import android.os.*;
+import android.support.v7.app.AppCompatActivity;
 import android.view.*;
 import android.graphics.*;
 import android.widget.*;
@@ -12,7 +13,7 @@ import android.provider.*;
 import com.microsoft.projectoxford.face.*;
 import com.microsoft.projectoxford.face.contract.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private final int PICK_IMAGE = 1;
     private ProgressDialog detectionProgressDialog;
 
@@ -24,39 +25,17 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
+        setContentView(R.layout.activity_main_real);
+        Button button1 = (Button)findViewById(R.id.button4);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(
-                        intent, "Select Picture"), PICK_IMAGE);
+                Intent intent = new Intent(getApplicationContext(), StatusCheck.class);
+                startActivity(intent);
             }
         });
 
         detectionProgressDialog = new ProgressDialog(this);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK &&
-                data != null && data.getData() != null) {
-            Uri uri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(
-                        getContentResolver(), uri);
-                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
-                imageView.setImageBitmap(bitmap);
-
-                // Uncomment
-                detectAndFrame(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
     // Detect faces by uploading a face image.
     // Frame faces after detection.
---
+++ app/src/main/java/com/example/omair/hcapp/StatusCheck.java
@@ -0,0 +1,16 @@
+package com.example.omair.hcapp;
+import android.app.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class StatusCheck extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_status);
+    }
+}
