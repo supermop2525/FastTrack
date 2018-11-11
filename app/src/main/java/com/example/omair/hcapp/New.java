fdff2aa7e83ca967e9b5254ba2c36ce087216217
supermop2525 <omair.sandhu@ryerson.ca>
2018-11-11 02:22:28 -0500

    All paths done

--- app/src/main/java/com/example/omair/hcapp/New.java
+++ app/src/main/java/com/example/omair/hcapp/New.java
@@ -1,12 +1,16 @@
 package com.example.omair.hcapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class New extends AppCompatActivity{
+    static final int REQUEST_IMAGE_CAPTURE1 = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +20,22 @@ public class New extends AppCompatActivity{
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(New.this,New2.class);
-                startActivity(intent);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE1);
+                }
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // add to sql server with faceid
+            Intent intent = new Intent(New.this, New2.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/omair/hcapp/New2.java
+++ app/src/main/java/com/example/omair/hcapp/New2.java
@@ -1,4 +1,25 @@
 package com.example.omair.hcapp;
 
-public class New2 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class New2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new2);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add to sql db
+                Intent intent = new Intent(New2.this,FinalPage.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
